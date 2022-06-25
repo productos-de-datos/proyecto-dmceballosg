de83938893c6daac35b454f5d4863824b6d0246a
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-25 17:29:13 -0500

    Punto 1

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -1,9 +1,8 @@
+import os
 def create_data_lake():
     """Cree el data lake con sus capas.
-
     Esta función debe crear la carpeta `data_lake` en la raiz del proyecto. El data lake contiene
     las siguientes subcarpetas:
-
     ```
     .
     |
@@ -18,11 +17,25 @@ def create_data_lake():
               |___ forecasts/
 
     ```
-
-
     """
-    raise NotImplementedError("Implementar esta función")
+    parent_dir = "data_lake"
+    os.chdir("../../")
+    cwd = os.getcwd()
+    path_data_lake = os.path.join(cwd, parent_dir)
 
+    if os.path.isdir(path_data_lake):
+        os.system("rm -rf "+ parent_dir)
+
+    os.makedirs(path_data_lake)
+    os.makedirs(path_data_lake + "/landing")
+    os.makedirs(path_data_lake + "/raw")
+    os.makedirs(path_data_lake + "/cleansed")
+    os.makedirs(path_data_lake + "/business")
+
+    os.makedirs(path_data_lake + "/business/reports")
+    os.makedirs(path_data_lake + "/business/reports/figures")
+    os.makedirs(path_data_lake + "/business/features")
+    os.makedirs(path_data_lake + "/business/forecasts")
 
 if __name__ == "__main__":
     import doctest
