dcc89043fa8a4e6462bf4f5078bbfe302b2fd295
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-30 21:52:13 -0500

    Intento linit

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,3 +1,6 @@
+"""
+Crea grafico de lineas que representa los precios promedios diarios.
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 def make_daily_prices_plot():
@@ -12,8 +15,6 @@ def make_daily_prices_plot():
     df_daily_prices = pd.read_csv("./data_lake/business/precios-diarios.csv")
     plt.plot(df_daily_prices["fecha"], df_daily_prices["precio"])
     plt.savefig('./data_lake/business/reports/figures/daily_prices.png')
-   
-
 
 if __name__ == "__main__":
     import doctest
